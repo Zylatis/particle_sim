cb296f0ba2eb4126810ecd43a4a3db8c05d25e1c
zylatis <graeme.gossel@gmail.com>
2019-12-26 20:43:04 +1100

    playing with boost aux libraries, experimenting with parallelising symmetric calc

--- src/io.h
+++ src/io.h
@@ -1,3 +1,5 @@
+#include "boost/filesystem.hpp" 
+namespace fs = boost::filesystem;
 
 void write_state( vector< vector< double>  >  &array, string file_name){
 
@@ -38,4 +40,31 @@ vector< vector<double> > FileRead( string filename){
         ++i;
     }
     return vvv;
+}
+
+
+void empty_folder( string folder_path, string extension){
+    // https://stackoverflow.com/questions/6935279/delete-all-txt-in-a-directory-with-c
+    // This needs to be extension-aware so we don't bin our .gitignore files
+    fs::path p(folder_path);
+    if(fs::exists(p) && fs::is_directory(p))
+    {
+        fs::directory_iterator end;
+        for(fs::directory_iterator it(p); it != end; ++it)
+        {
+            try
+            {
+                if(fs::is_regular_file(it->status()) && (it->path().extension().compare(extension) == 0))
+                {
+                    fs::remove(it->path());
+
+                }
+            }
+            catch(const std::exception &ex)
+            {
+                ex;
+            }
+        }
+    }
+
 }
--- src/plummer_collision.cpp
+++ src/plummer_collision.cpp
@@ -1,9 +1,10 @@
 using namespace std;
 
 #include "imports.h"
-#include "math_objs.h"
 #include "io.h"
-// #include "include/cxxopts.hpp"
+#include "boost/program_options.hpp" 
+
+namespace po = boost::program_options;
 
 int FILE_P = 15; // precision of outputs
 int n;
@@ -30,12 +31,15 @@ vector<vector< double > > force( const vector<vector< double > > &pos_vec ){
 	
 	#pragma omp parallel for
 	for(int i = 0; i< n; i++){
+		// for(int j = i+1; j<n; j++){
 		for(int j = 0; j<n; j++){
 			if(i!=j){
 				vector<double> drvec = diff_vec(pos_vec, j, i);				
 				double rmag = get_mag(drvec);
 				for(int k = 0; k<3;k++){
 					out[i][k] += G*mass*drvec[k]/(eps+pow(rmag,3));
+					// #pragma omp atomic
+					// out[j][k] += -G*mass*drvec[k]/(eps+pow(rmag,3));
 				}
 			}
 		}
@@ -96,31 +100,40 @@ vector<vector<double> > init(  default_random_engine &rands, const vector<double
 
 // Main simulation
 int main ( int argc, char *argv[] ){
+	po::options_description desc("Options"); 
+    desc.add_options() 
+      ("help", "Print help messages") 
+      ("add", "additional options") 
+      ("like", "this"); 
 
-
-
+    empty_folder("output/data/",".dat");
+	empty_folder("output/plots/",".png");
+	
 	omp_set_num_threads( 4 );
 	srand(1);
 	default_random_engine rands;
-	n = 1000;
+	n = 200;
 	mass = 1./n;
 	int step(0), file_n(0);
 	double dt(0.1), t(0.), tmax(200.);
 	vector<double> blank(3, 0.);
 	vector<vector< double > > pos(n,blank), vel(n,blank), accel(n,blank);
 	
+	// Lazy setup of cluster 1
 	for(int i = 0; i<n/2; i++){	
 		vector<vector<double> > temp = init( rands,{0,0,0},{0,0,0});
 		pos[i] = temp[0];
 		vel[i] = temp[1];
 	}
-	
+
+	// Lazy setup of cluster 2	
 	for(int i = n/2; i<n; i++){	
 		vector<vector<double> > temp = init( rands,{30.,0,0},{-.02,0,0});
 		pos[i] = temp[0];
 		vel[i] = temp[1];
 	}
 	
+	// Initial t0 leapfrog step
 	accel = force(pos);
 	for(int i = 0; i<n; i++){
 		for(int k = 0; k<3;k++){
@@ -137,11 +150,10 @@ int main ( int argc, char *argv[] ){
 		if(step%10==0){
 			cout<<t<<endl;
 			temp = pos;
-			//~ auto out = async(launch::async, write_state, ref( temp ),to_string(file_n)+"_pos");
 			write_state(pos, to_string(file_n)+"_pos");
 			file_n++;
 		}
-		t+=dt;
+		t += dt;
 		step++;
 	}
 	cout<< ( get_wall_time() - wt )<<endl;
