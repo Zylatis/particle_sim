a2a3ede22f3837151e88a055b518b7021892fdd2
zylatis <graeme.gossel@gmail.com>
2019-12-26 21:12:12 +1100

    fix n-particle bug in plot.py

--- src/scripts/plot.py
+++ src/scripts/plot.py
@@ -3,12 +3,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import random
 import pandas as pd
 from random import randrange, uniform
-n = 200
-sizes = [ uniform(0, 1) for i in range(n) ]
+n = len(pd.read_csv("../../output/data/"+str(0)+"_pos.dat", sep='\t'))
+sizes = [ uniform(1, 2) for i in range(n) ]
 
 for i in range(1000):
 	print i
-	fig = pyplot.figure(figsize=pyplot.figaspect(1)*2)
+	fig = pyplot.figure(figsize=pyplot.figaspect(1)*3)
 	ax = Axes3D(fig)
 	ax.set_xlim3d(-20,20)
 	ax.set_ylim3d(-20,20)
