6575dc8fb5413dcce161b790404e93bd01cd0e64
Graeme <graeme.gossel@gmail.com>
2020-12-19 12:24:08 +0800

    Tidy integrator code remove problematic sym-force-parallel thing, but leave note

--- src/integrator.h
+++ src/integrator.h
@@ -14,41 +14,22 @@ void calc_force_strided( const vector<current_dtype> &strided_pos_vec, const vec
 	
 	#pragma omp parallel for schedule(static)
 	for(int i = 0; i<n;i++){
-		for(int j = i+1; j<n; j++){ // Use symmetry of force
-		// for(int j = 0; j<n; j++){
+		// for(int j = i+1; j<n; j++){ // Use symmetry of force
+		for(int j = 0; j<n; j++){
 			if(i!=j){
 				array<current_dtype,3> drvec;
 				for(int k = 0; k<3;k++){
-					// cout<<3*thread_id+k<<endl;
 					drvec[k] = strided_pos_vec[3*j+k] - strided_pos_vec[3*i+k];
 					// NOTE: tried to be clever here and preallocate the dr vector to see if faster as fewer mem ops but possibly introduced false sharing?
 					// strided_dt_threadcpy[3*thread_id+k] = strided_pos_vec[3*j+k] - strided_pos_vec[3*i+k];
 				}
-
-				// if(thread_id==0){
-				// 	cout<<drvec[0] - strided_dt_threadcpy[3*thread_id + 0]<<endl;
-				// 	cout<<drvec[1] - strided_dt_threadcpy[3*thread_id + 1]<<endl;
-				// 	cout<<drvec[2] - strided_dt_threadcpy[3*thread_id + 2]<<endl;
-				// }
-			
 				current_dtype rmag = sqrt(drvec[0]*drvec[0] + drvec[1]*drvec[1] + drvec[2]*drvec[2]);
-				// current_dtype rmag2 = sqrt(strided_dt_threadcpy[3*thread_id + 0]*strided_dt_threadcpy[3*thread_id + 0] + strided_dt_threadcpy[3*thread_id + 1]*strided_dt_threadcpy[3*thread_id + 1] + strided_dt_threadcpy[3*thread_id + 2]*strided_dt_threadcpy[3*thread_id + 2]);
-			
-				// if(thread_id==0){
-				// 	// cout<<rmag-rmag2<<endl;
-				// }
-			
 				for(int k = 0; k<3;k++){
 					current_dtype val = G*mass*drvec[k]/(eps+rmag*rmag*rmag);
-					// current_dtype val2 = G*mass*strided_dt_threadcpy[3*thread_id+k]/(eps+rmag2*rmag2*rmag2);
 
-					// if(thread_id==0){
-						// if( abs(val-val2)> 0.000001){
-							// cout<<val-val2<<endl;
-						// }
-					// }
-					// strided_force_threadcpy[0][3*i+k] += val;
-					strided_force_threadcpy[0][3*j+k] -= val;
+					strided_force_threadcpy[0][3*i+k] += val;
+					// Use below if using symmetry of force aspect but seems to break when threaded
+					// strided_force_threadcpy[0][3*j+k] -= val;
 				}
 			}
 		}
